c885cbc371df421f96ae6f5505c2e5d18ae8cb7a
MichaelACohen <michael@cohenhome.net>
2014-11-02 23:49:11 -0800

    made very basic profile page view, updated links

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
+app.get('/profile', function(req, res) {
+	res.render('pages/profile');
+});
+
 app.get('/events', function(req, res) {
     connection.query('SELECT * FROM event', function(err, rows, fields) {
         res.render('pages/events', { events : rows });
