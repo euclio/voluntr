ff6c8cabf43fa813576308065a32743d1dba4fe5
MichaelACohen <michael@cohenhome.net>
2014-11-04 10:34:44 -0800

    add event page
    put link on navbar for now

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -25,6 +28,23 @@ app.get('/events', function(req, res) {
         res.render('pages/events', { events: rows, moment: moment });
     });
 });
+
+app.get('/add', function(req, res) {
+    res.render('pages/addevent');
+});
+
+
+app.post('/events', function(req, res) {
+    var query = "INSERT INTO event (title, description, location, startTime, endTime) VALUES (?, ?, ?, ?, ?)";
+    connection.query(query, [req.body.title, req.body.description, req.body.location, req.body.startTime, req.body.endTime], function(err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success");
+        }
+    });
+});
+
 
 app.use('/static', express.static(__dirname + '/static'));
 
