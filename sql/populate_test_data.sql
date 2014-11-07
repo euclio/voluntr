6dfb39f2d6ab2ac99c88c4945b8fbf9784fb292a
MichaelACohen <michael@cohenhome.net>
2014-11-07 00:22:59 -0800

    adding title as field to event table

--- sql/populate_test_data.sql
+++ sql/populate_test_data.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS event;
 CREATE TABLE event (eventID INT NOT NULL AUTO_INCREMENT,
+                    title VARCHAR(50) NOT NULL,
                     description VARCHAR(300) NOT NULL,
                     location VARCHAR(50) NOT NULL,
                     startTime DATETIME NOT NULL,
@@ -15,12 +16,14 @@ CREATE TABLE users (userID INT NOT NULL AUTO_INCREMENT,
                     PRIMARY KEY (userID));
 
 INSERT INTO event VALUES (0,
+                          'Event title 1',
                           'This event is awesome.',
                           'Pomona College',
                           '2014-10-28 11:00:00',
                           '2014-10-28 12:00:00');
 
 INSERT INTO event VALUES (0,
+                          'Event title 2',
                           'This event is also awesome.',
                           'Pomona College',
                           '2014-10-31 11:00:00',
