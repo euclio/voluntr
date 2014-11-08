91c600ec3fee43debed0ec704ebb9d5b24efc106
MichaelACohen <michael@cohenhome.net>
2014-11-08 14:04:57 -0800

    add event form with validation finished except for date picker

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var nconf = require('nconf');
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
+var forms = require('forms');
 
 nconf.argv()
      .env();
@@ -30,9 +31,52 @@ app.use(function(req, res, next) {
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    database: 'test',
+    database: 'test'
 });
 connection.connect();
+
+var fields = forms.fields,
+    validators = forms.validators,
+    widgets = forms.widgets;
+
+var add_event_form = forms.create({
+    title: fields.string({
+        required: validators.required('Title is required.'),
+        widget: widgets.text({ classes: ['input-with-feedback'] }),
+        errorAfterField: true,
+        validators: [validators.maxlength(50)]
+    }),
+    description: fields.string({
+        required: validators.required('Description is required.'),
+        widget: widgets.textarea({rows:4}),
+        errorAfterField: true,
+        validators: [validators.maxlength(300)]
+    }),
+    location: fields.string({
+        required: validators.required('Location is required.'),
+        widget: widgets.text({ classes: ['input-with-feedback'] }),
+        errorAfterField: true,
+        validators: [validators.maxlength(50)]
+    })
+    /*
+    startDate: fields.date({
+        required: true,
+        widget: widgets.date(),
+        errorAfterField: true,
+        validators: [validators.rangelength(10,10)]
+    })
+    */
+});
+
+var bootstrapField = function (name, object) {
+    object.widget.classes = object.widget.classes || [];
+    object.widget.classes.push('form-control');
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="alert alert-error">' + object.error + '</div>' : '';
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
 
 passport.serializeUser(function(user, done) {
     // TODO: Save user session to database?
@@ -93,18 +137,67 @@ app.get('/events', function(req, res) {
 });
 
 app.get('/add', function(req, res) {
-    res.render('pages/addevent');
+    res.render('pages/addevent', {
+        form: add_event_form.toHTML(bootstrapField)
+    });
 });
 
-app.post('/events', function(req, res) {
-    var query = "INSERT INTO event (title, description, location, startTime, endTime) VALUES (?, ?, ?, ?, ?)";
-    connection.query(query, [req.body.title, req.body.description, req.body.location, req.body.startTime, req.body.endTime], function(err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("success");
-        }
+//returns true on success, false otherwise
+function handleFormData(req, res) {
+    var ret = false;
+    add_event_form.handle(req, {
+        success: function (form) {
+            ret = true;
+            res.render('pages/addevent', {
+                form: form.toHTML()
+            });
+        },
+        error: function (form) {
+            res.render('pages/addevent', {
+                form: form.toHTML()
+            });
+        },
+        empty: function (form) { }
     });
+    return ret;
+}
+
+var DEFAULT_DATE = "2014-11-30"
+app.post('/add', function(req, res) {
+    var success = handleFormData(req, res);
+    if (success) {
+        var formatHours = function(hours, ampm) {
+            var intHours = parseInt(hours)
+            if (ampm == "PM") {
+                if (intHours != 12) {
+                    intHours += 12
+                }    
+            } else {
+                if (intHours == 12) {
+                    intHours -= 12
+                }
+            }
+            hours = intHours.toString()
+            if (intHours < 10) {
+                hours = "0" + hours
+            }
+            return hours
+        }
+        var startHours = formatHours(req.body.hours[0], req.body.ampm[0])
+        var startTime = DEFAULT_DATE + " " + startHours + req.body.minutes[0] + ":00"
+        
+        var endHours = formatHours(req.body.hours[1], req.body.ampm[1])
+        var endTime = DEFAULT_DATE + " " + endHours + req.body.minutes[1]
+    
+        var query = "INSERT INTO event (title, description, location, startTime, endTime) VALUES (?, ?, ?, ?, ?)"
+        connection.query(query, [req.body.title, req.body.description, req.body.location, startTime, endTime], function(err, res) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("success")
+            }
+        })
+    }
 });
 
 app.get('/register', function(req, res) {
