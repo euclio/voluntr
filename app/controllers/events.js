0ccdc8cb2032577fd094646185e7f7a51e4dc834
MichaelACohen <michael@cohenhome.net>
2014-11-20 12:21:14 -0800

    added datepicker

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -53,11 +53,12 @@ exports.create = function(req, res) {
                 }
                 return hours;
             };
+
             var startHours = formatHours(req.body.hours[0], req.body.ampm[0]);
-            var startTime = DEFAULT_DATE + " " + startHours + req.body.minutes[0] + ":00";
+            var startTime = req.body.date[0] + " " + startHours + req.body.minutes[0] + ":00";
 
             var endHours = formatHours(req.body.hours[1], req.body.ampm[1]);
-            var endTime = DEFAULT_DATE + " " + endHours + req.body.minutes[1];
+            var endTime = req.body.date[1] + " " + endHours + req.body.minutes[1];
 
             var query = "INSERT INTO event \
                         (title, description, location, startTime, endTime) \
