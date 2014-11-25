49c2a5adc80aac5c82da9a12385ec2a4bbce5428
MichaelACohen <michael@cohenhome.net>
2014-11-24 22:21:25 -0800

    validate that start time is before end time when adding event

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -32,50 +32,85 @@ exports.new = function(req, res) {
     });
 };
 
-var DEFAULT_DATE = "2014-11-30";
+var toMilitaryTime = function(hours, ampm) {
+    if (ampm == "AM") {
+        if (hours == 12) {
+            hours = 0;
+        }
+    } else {
+        if (hours != 12) {
+            hours += 12;
+        }
+    }
+    return hours;
+};
+
+//make sure start date is before end date
+var validateStartBeforeEnd = function(start, end) {
+    if (moment(start.date).isBefore(end.date)) {
+        return true;
+    } else if (moment(start.date).isSame(end.date)) {
+        startHours = toMilitaryTime(start.hours, start.ampm);
+        endHours = toMilitaryTime(end.hours, end.ampm);
+        if (startHours > endHours) {
+            return false;
+        } else if (startHours == endHours) {
+            if (start.minutes >= end.minutes) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+};
+
 exports.create = function(req, res) {
+    var reqobj = req;
     forms.addEventForm.handle(req, {
         success: function(form) {
-            var formatHours = function(hours, ampm) {
-                var intHours = parseInt(hours);
-                if (ampm == "PM") {
-                    if (intHours != 12) {
-                        intHours += 12;
-                    }
-                } else {
-                    if (intHours == 12) {
-                        intHours -= 12;
-                    }
-                }
-                hours = intHours.toString();
-                if (intHours < 10) {
-                    hours = "0" + hours;
-                }
-                return hours;
+            var start = {
+                date: reqobj.body.date[0],
+                hours: reqobj.body.hours[0],
+                minutes: reqobj.body.minutes[0],
+                ampm: reqobj.body.ampm[0]
             };
+            var end = {
+                date: reqobj.body.date[1],
+                hours: reqobj.body.hours[1],
+                minutes: reqobj.body.minutes[1],
+                ampm: reqobj.body.ampm[1]
+            }
+            if (!validateStartBeforeEnd(start, end)) {
+                req.flash("error", 'Start time must be before end time.');
+                res.redirect('/add');
+            } else {
+                var startHours = toMilitaryTime(parseInt(req.body.hours[0]), req.body.ampm[0]);
+                var startTime = req.body.date[0] + " " + startHours + req.body.minutes[0] + ":00";
 
-            var startHours = formatHours(req.body.hours[0], req.body.ampm[0]);
-            var startTime = req.body.date[0] + " " + startHours + req.body.minutes[0] + ":00";
+                var endHours = toMilitaryTime(parseInt(req.body.hours[1]), req.body.ampm[1]);
+                var endTime = req.body.date[1] + " " + endHours + req.body.minutes[1];
 
-            var endHours = formatHours(req.body.hours[1], req.body.ampm[1]);
-            var endTime = req.body.date[1] + " " + endHours + req.body.minutes[1];
-
-            var query = "INSERT INTO event \
+                var query = "INSERT INTO event \
                         (title, description, location, startTime, endTime) \
                         VALUES (?, ?, ?, ?, ?)";
-            database.query(query,
+                database.query(query,
                            [req.body.title,
                             req.body.description,
                             req.body.location,
                             startTime,
                             endTime], function(err, dbRes) {
-                if (err) {
-                    throw err;
-                } else {
-                    req.flash('success', 'Event successfully added.');
-                    res.redirect('/add');
-                }
-            });
+                    if (err) {
+                        throw err;
+                    } else {
+                        req.flash('success', 'Event successfully added.');
+                        res.redirect('/add');
+                    }
+                });
+            }
         },
         other: function(form) {
             res.redirect('/add');
