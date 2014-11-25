2925f43f9bb5598dcf8de704b44f913d21dd4586
Andy Russell <arussell123@gmail.com>
2014-11-25 10:04:26 -0800

    Revert "use new table name for user"
    This reverts commit d6f050d48d78a6eb933cc92544e4ff7dca5b4201.

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport, config) {
     });
 
     passport.deserializeUser(function(userID, done) {
-        database.query('SELECT * FROM user WHERE userID = ?',
+        database.query('SELECT * FROM users WHERE userID = ?',
                          [userID], function(err, rows, fields) {
             if (err) { return done(err); }
             return done(null, rows[0]);
@@ -22,7 +22,7 @@ module.exports = function(passport, config) {
         },
         function(email, password, done) {
             // Attempt to find the email in the database.
-            database.query('SELECT * FROM user WHERE email = ? LIMIT 1',
+            database.query('SELECT * FROM users WHERE email = ? LIMIT 1',
                              [email], function(err, rows, fields) {
                 if (err) { return done(err); }
                 if (rows.length === 0) {
